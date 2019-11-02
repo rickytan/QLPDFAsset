61fbb2658c94fef59acf0eff65d207348d6aa0c6
hztanxin <hztanxin@corp.netease.com>
2019-11-02 14:57:43 +0800

    Initial Commit

---
+++ QLPDFAsset/QLPDFAsset.h
@@ -0,0 +1,14 @@
+//
+//  QLPDFAsset.h
+//  QLPDFAsset
+//
+//  Created by Ricky on 2019/11/2.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface QLPDFAsset : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ QLPDFAsset/QLPDFAsset.m
@@ -0,0 +1,16 @@
+//
+//  QLPDFAsset.m
+//  QLPDFAsset
+//
+//  Created by Ricky on 2019/11/2.
+//
+
+#import "QLPDFAsset.h"
+
+@implementation QLPDFAsset
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
